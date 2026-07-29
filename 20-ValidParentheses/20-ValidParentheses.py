10b660050151c3deabde7317e8319d96027933e6
Bennett Davis <138383741+bennettdavisv2@users.noreply.github.com>
2026-07-29 10:13:58 -0400

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,17 @@
+# Last updated: 7/29/2026, 10:13:58 AM
+class Solution:
+    def isValid(self, s: str) -> bool:
+     stack = []
+     closeToOpen = {")" : "(" , "]" : "[", "}" : "{"}
+     for char in s:
+        if char in closeToOpen:
+            if stack and stack[-1] == closeToOpen[char]:
+                stack.pop()
+            else:
+                return False
+        else:
+         stack.append(char)
+
+     return True if not stack else False    
+
+          
