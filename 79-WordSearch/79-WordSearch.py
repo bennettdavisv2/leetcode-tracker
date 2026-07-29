4fcba30878953702252160b50ea52958726376d9
Bennett Davis <138383741+bennettdavisv2@users.noreply.github.com>
2026-07-29 10:13:43 -0400

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.py
@@ -0,0 +1,25 @@
+# Last updated: 7/29/2026, 10:13:42 AM
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def backtrack(i, j, k):
+            # 2 base cases; one for found and one for illegal bounds
+            if k == len(word):
+                return True
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+                return False
+            
+            # 
+            temp = board[i][j]
+            board[i][j] = ''
+            
+            if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
+                return True
+            
+            board[i][j] = temp
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
+        return False
