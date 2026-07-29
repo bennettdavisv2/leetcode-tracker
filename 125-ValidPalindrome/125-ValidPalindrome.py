ff6d4bcce5bba7ab7afbc3131411ba8c18fc40f4
Bennett Davis <138383741+bennettdavisv2@users.noreply.github.com>
2026-07-29 10:13:44 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,19 @@
+# Last updated: 7/29/2026, 10:13:43 AM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
