8e4216ee65d237916e5c16becae0ab263eec513b
Bennett Davis <138383741+bennettdavisv2@users.noreply.github.com>
2026-07-29 10:13:45 -0400

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/29/2026, 10:13:44 AM
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int left = 0;
+        int right = rows * cols - 1;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];  // Find midValue using division and modulus
+            
+            if(midValue == target) {
+                return true;
+            } else if(midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
